7bddbbb5e37c417a20f62a207f640bc564bfab4e
YS <kerol2r20@gmail.com>
2025-12-28 01:15:32 +0800

    Fix style issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,22 +6,33 @@ import tseslint from 'typescript-eslint';
 import stylistic from '@stylistic/eslint-plugin';
 
 export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
-  {
-    plugins: {
-      '@stylistic': stylistic,
+    eslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+    {
+        'rules': {
+            '@typescript-eslint/no-empty-function': 'off',
+            '@typescript-eslint/no-useless-constructor': 'off',
+            'no-useless-constructor': 'off',
+            'no-empty-function': 'off',
+        },
     },
-    rules: {
-      '@stylistic/semi': ['error', 'always'],
-      '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
-      '@stylistic/no-trailing-spaces': ['error'],
-      '@stylistic/comma-dangle': ['error', 'always-multiline'],
-      '@typescript-eslint/no-unused-vars': ['error', {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-      }],
+    {
+        plugins: {
+            '@stylistic': stylistic,
+        },
+        rules: {
+            '@stylistic/semi': ['error', 'always'],
+            '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+            '@stylistic/no-trailing-spaces': ['error'],
+            '@stylistic/comma-dangle': ['error', 'always-multiline'],
+            '@typescript-eslint/no-unused-vars': ['error', {
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+            }],
+            '@stylistic/indent': ['error', 4],
+            '@stylistic/jsx-indent': ['error', 4],
+            '@stylistic/jsx-indent-props': ['error', 4],
+        },
     },
-  },
 );
